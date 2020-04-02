b0c38ac792eb917d2b4c6009724386f6cb0ce4ba
kdyla <kdyla@griddynamics.com>
2020-04-02 10:44:44 +0200

    add the rest of order process with assertions. works on chrome and firefox

--- tests/addToCartTest.js
+++ tests/addToCartTest.js
@@ -3,6 +3,21 @@ module.exports = {
     'Add To Cart Test'(browser) {
         const url = browser.globals.mainPage;
         const page = browser.page.mainPage();
+        const correctEmailAddress = browser.globals.registeredEmail;
+        const correctPassword = 'password';
+        const checkoutHeader = 'h1[id="cart_title"]';
+        const checkoutHeaderText = 'SHOPPING-CART SUMMARY';
+        const addressHeader = 'h1[class="page-heading"]';
+        const addressHeaderText = 'ADDRESSES';
+        const shippingHeader = 'h1[class="page-heading"]';
+        const shippingHeaderText = 'SHIPPING';
+        const paymentHeader = 'h1[class="page-heading"]';
+        const paymentHeaderText = 'PLEASE CHOOSE YOUR PAYMENT METHOD';
+        const emptyCartAlertBox = 'p[class="alert alert-warning"]';
+        const payByBankWire = 'a[title="Pay by bank wire"]';
+        const payByCheck = 'a[title="Pay by check."]';
+        const bankWireBody = 'body[id="module-bankwire-payment"]';
+        const orderConfirmationBody = 'body[id="order-confirmation"]';
 
         page
             .navigate(url)
@@ -10,7 +25,50 @@ module.exports = {
             .searchDress()
             .chooseDress()
             .addToCart()
-            .proceedToCkeckout()
-            .pause(3000);
+            .proceedToCheckout()
+            .pause(1000);
+
+        browser
+            .assert.visible(checkoutHeader, "Checkout page is visible")
+            .assert.containsText(checkoutHeader, checkoutHeaderText, "Header text is correct")
+            .assert.not.visible(emptyCartAlertBox, "Shopping cart is not empty");
+
+        page
+            .proceedToSignIn()
+            .setLoginAndPassword(correctEmailAddress, correctPassword)
+            .submitLogin(browser);
+
+        browser
+            .assert.visible(addressHeader, "Address page is visible")
+            .assert.containsText(addressHeader, addressHeaderText, "Header text is correct");
+
+        page
+            .proceedToShipping()
+            .checkTermsOfService();
+
+        browser
+            .assert.visible(shippingHeader, "Shipping page is visible")
+            .assert.containsText(shippingHeader, shippingHeaderText, "Header text is correct");
+
+        page
+            .proceedToPayment();
+
+        browser
+            .assert.visible(paymentHeader, "Payment page is visible")
+            .assert.containsText(paymentHeader, paymentHeaderText, "Header text is correct")
+            .assert.visible(payByBankWire, "Pay by bank wire option is available")
+            .assert.visible(payByCheck, "Pay by check option is available");
+
+        page
+            .payByBankWire();
+
+        browser
+            .assert.visible(bankWireBody, "Bank wire payment page is visible");
+
+        page
+            .confirmOrder();
+
+        browser
+            .assert.visible(orderConfirmationBody, "Order confirmation page is visible");
     }
 };
